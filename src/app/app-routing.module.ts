81c1e670a81ac0bc2f18e033aeff4a8cd38b7496
montassar95 <montassar.trabelsi95@gmail.com>
2024-06-18 12:22:49 +0100

    test Projet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Route par défaut
+  { path: 'home', component: HomeComponent }, // Route vers HomeComponent
+
+  // Ajoutez d'autres routes ici au besoin
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
